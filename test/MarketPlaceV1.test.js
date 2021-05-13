1642dee98b165db59a92c4ecbe34e11f58fee24f
capitanwesler <guillermo.campanudo@hotmail.com>
2021-05-13 21:37:20 +0100

    Making some tests and updating .gitignore

--- test/MarketPlaceV1.test.js
+++ test/MarketPlaceV1.test.js
@@ -1,5 +1,22 @@
-const { ethers } = require('hardhat');
+const { ethers, upgrades } = require('hardhat');
+const assert = require('assert');
 
-before(async () => {});
+let marketPlaceV1;
 
-describe(async () => {});
+before(async () => {
+  const MarketPlaceV1 = await ethers.getContractFactory('MarketPlaceV1');
+  marketPlaceV1 = await upgrades.deployProxy(
+    MarketPlaceV1,
+    ['0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'],
+    { initializer: 'initialize' }
+  );
+  await marketPlaceV1.deployed();
+});
+
+describe('Testing the NFT MarketPlaceV1', () => {
+  it('deployed correctly without error', async () => {});
+
+  it('print the address of the marketPlace', async () => {
+    assert.ok(marketPlaceV1.address);
+  });
+});
